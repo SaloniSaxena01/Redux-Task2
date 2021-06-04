98e8217ef6e6b1075372cdb7e5458d731be3c3ab
SaloniSaxena01 <60663789+SaloniSaxena01@users.noreply.github.com>
2021-06-04 16:48:07 +0530

    Add files via upload

--- src/redux/action/temperatureActions.js
+++ src/redux/action/temperatureActions.js
@@ -1,1 +1,16 @@
+import {
+    FETCH_TEMPERATURE_REQUEST,
+    FETCH_TEMPERATURE_SUCCESS, 
+    FETCH_TEMPERATURE_FAILURE
+} from './temperatureTypes'
+
+
+
+export const fetchTemperatureSuccess = temp => {
+    return {
+        type: FETCH_TEMPERATURE_SUCCESS,
+        payload: temp
+    }
+}
+
 
