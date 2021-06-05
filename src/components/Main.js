bb399e9769db7ea66bea61a5ef50204cd7552848
SaloniSaxena01 <60663789+SaloniSaxena01@users.noreply.github.com>
2021-06-05 14:02:07 +0530

    Add files via upload

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,16 +5,26 @@ import { fetchTemperatureSuccess } from './temperatureActions'
 import "../css/main.css"
 
 const Main = ({ tempData, fetchTemperatureSuccess }) => {
-    useEffect(() => {
-       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Kanpur&appid=bf18c965315fddbe8b09511c40fc1a15`)
-       .then((response)=>fetchTemperatureSuccess(response.data.main.temp))
-       .catch((err)=>console.log(err)) 
-    }, [])
+    const [city,setCity] = useState("");
+    function clickHandler(event){
+        setCity(event.target.value);
+    }
+    const handleSubmit = () => {
+       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bf18c965315fddbe8b09511c40fc1a15`)
+       .then((response)=>{
+        fetchTemperatureSuccess(response.data.main.temp+" Degree Celcius")
+    })
+    .catch((err)=>fetchTemperatureSuccess("City not found"))
+}
     
     return (<div className="main container-fluid">
             
     <i className="fas fa-sun first"></i>
-    <h3>{tempData.temp} Degree Celcius</h3>
+            <div className="input-area">
+                <input id="userInput" type="text" placeholder="Enter city name" value={city} onChange = { (event) => clickHandler(event)}></input>
+                <button className="button-container" onClick = { handleSubmit } >Search</button>
+            </div>
+    <h3>{tempData.temp}{tempData.name}</h3>
     <i className="fas fa-sun second"></i>
 
 
